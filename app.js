132980a30487175ad7b5fd84195c26a39ca85b19
StefaniaSuar <stefaniasuarez1026@gmail.com>
2025-09-09 00:05:03 -0300

    add final proyect

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let numeroUsuario  = document.getElementById('amigo').value.trim();
+
+    if (numeroUsuario === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(numeroUsuario);
+    console.log(amigos);
+    limpiarCaja();
+
+    listarAmigos();
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function listarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega al menos uno.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+Aplicación web que permite agregar amigos a una lista y sortear uno de manera aleatoria como amigo secreto.
+ Hecha con HTML, CSS y JavaScript.
+
+
+Funcionalidades
+Agregar amigos a la lista.
+Validar que no se ingresen nombres vacíos.
+Mostrar la lista de amigos en pantalla.
+Sortear un amigo secreto y mostrar el resultado.
+
+
+Cómo usar
+Abrir index.html en el navegador.
+Escribir un nombre y hacer clic en Añadir.
+Hacer clic en Sortear amigo para elegir un amigo secreto.
